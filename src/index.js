910bc26fd21688d3dca9354933c805c41de36196
M1atias <maty.f.molina4a@gmail.com>
2021-11-18 13:58:13 -0300

    config Server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 const express = require('express');
-express();
+const config = require('./server/config');
+
+//database
+require('./database');
+
+const app = config(express());
+
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
