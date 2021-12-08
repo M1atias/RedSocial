fb9758da7ff1eadb3f46711bdd92202d011d5208
M1atias <maty.f.molina4a@gmail.com>
2021-12-08 11:33:29 -0300

    add: Se guardan las images en la bd

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,10 +1,44 @@
 const ctrl = {};
+const path = require('path');
+const {randomNumber} = require('../helpers/libs');
+const fs = require('fs-extra');
+
+const {Image} = require('../models/index');
 
 ctrl.index = (req,res) =>{
 
 };
 
 ctrl.create = (req,res) =>{
+
+    const saveImage = async () => {
+        const imageURL = randomNumber();
+        const images = await Image.find({filename: imageURL});
+        if (images.length > 0) {    
+            saveImage();
+        }else{
+            console.log(imageURL);
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const targetPath = path.resolve(`src/public/upload/${imageURL}${ext}`)
+
+            if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+                await fs.rename(imageTempPath, targetPath);
+                const newImg = new Image({
+                    title: req.body.title,
+                    filename: imageURL + ext,
+                    description: req.body.description
+                });
+                const imageSaved = await newImg.save();
+                res.send('works');
+            } else{
+                await fs.unlink(imageTempPath);
+                res.status(500).json({error:'Only images are allowed'});
+            }
+        }
+    };
+    saveImage();
+    
     
 };
 
